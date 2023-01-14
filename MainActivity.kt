22d3daa93c7d8c66b8778f33549f42c291943cd7
miknam <m.nambu@wakka.co>
2023-01-14 21:45:12 +0900

    よろしくお願いいたします。

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.EditText
 
@@ -13,10 +14,16 @@ class MainActivity : AppCompatActivity() {
     }
     /* Sendボタン押下時 */
     fun sendMessage(view: View) {
-        val intent: Intent = Intent(this@MainActivity,
-            DisplayMessageActivity::class.java)
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            val editText: EditText = findViewById(R.id.edittext) as EditText
+            val text: String? = editText.text.toString()
+            putExtra(“KEY”, text)
+        }
         val editText: EditText = findViewById(R.id.edittext) as EditText
-        intent.putExtra(“KEY”, editText.text.toString())
+        val text: String? = editText.text.toString()
+        intent.putExtra(“KEY”, text)
         startActivity(intent)
     }
 }
+
+
